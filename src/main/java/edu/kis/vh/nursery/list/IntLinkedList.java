d0370ed8f584ac7f9f7a512d45d31f1f29cbfbad
Student258491 <258491@edu.p.lodz.pl>
2026-04-11 23:20:59 +0200

    14.Usunęto plik Node.java i utworzono klassę Node w IntLinkedList.

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,7 +1,36 @@
 package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
+    private class Node {
 
+        private final int value;
+        private Node prev;
+        private Node next;
+
+        public Node(int i) {
+            value = i;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public Node getPrev() {
+            return prev;
+        }
+
+        public void setPrev(Node prev) {
+            this.prev = prev;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+    }
     private static final int EMPTY_RHYMER_INDICATOR = -1;
     private Node last;
     private int i;
@@ -37,5 +66,4 @@ public class IntLinkedList {
         last = last.getPrev();
         return ret;
     }
-
 }
